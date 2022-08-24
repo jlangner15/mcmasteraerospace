c6161b2dcd4478529047c17d1c3ab231a830a37e
jlangner15 <langnerj@mcmaster.ca>
2022-08-23 22:00:22 -0400

    Coming soon

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './images/mastLogo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" />
+        <p>
+          <b>Coming Soon</b>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
