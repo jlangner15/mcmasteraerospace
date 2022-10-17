66df6c28b9eafbe1115a6107d95972c4fe626ea2
Jeremy Langner <langnerj@mcmaster.ca>
2022-10-17 01:35:45 -0400

    Updates

---
+++ precache-manifest.7cc7abf7c59764f1ebb3860afb2445ad.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb4d3a568398d919f111",
+    "url": "/mcmasteraerospace/static/css/main.4cad64eb.chunk.css"
+  },
+  {
+    "revision": "eb4d3a568398d919f111",
+    "url": "/mcmasteraerospace/static/js/main.eb4d3a56.chunk.js"
+  },
+  {
+    "revision": "bb2d15f8b100106d30b0",
+    "url": "/mcmasteraerospace/static/js/1.bb2d15f8.chunk.js"
+  },
+  {
+    "revision": "fcc9517fe3c2da6f93b9",
+    "url": "/mcmasteraerospace/static/css/2.f454a78b.chunk.css"
+  },
+  {
+    "revision": "fcc9517fe3c2da6f93b9",
+    "url": "/mcmasteraerospace/static/js/2.fcc9517f.chunk.js"
+  },
+  {
+    "revision": "db39fcff7ce488319b8a",
+    "url": "/mcmasteraerospace/static/js/runtime~main.db39fcff.js"
+  },
+  {
+    "revision": "860c0e74436a060d01c5a5feb945fa89",
+    "url": "/mcmasteraerospace/static/media/McMaster- plane-colour.860c0e74.png"
+  },
+  {
+    "revision": "ee761be490ce81562cebb66c419cc10b",
+    "url": "/mcmasteraerospace/static/media/McMaster-title-color.ee761be4.png"
+  },
+  {
+    "revision": "f2dcc57b196f37ae020cca170aa9e84f",
+    "url": "/mcmasteraerospace/static/media/SAE_International_logo.f2dcc57b.png"
+  },
+  {
+    "revision": "af9d7496fe52d1a100831f9a16cacd95",
+    "url": "/mcmasteraerospace/static/media/SAE_design_east.af9d7496.png"
+  },
+  {
+    "revision": "8363a0bcaa5d0a745f9833e2738826cb",
+    "url": "/mcmasteraerospace/static/media/around-the-world.8363a0bc.png"
+  },
+  {
+    "revision": "accae175d519c5dbd7095b25d95c63e6",
+    "url": "/mcmasteraerospace/static/media/team.accae175.png"
+  },
+  {
+    "revision": "8adb877f8c035c53823b7926e83d0119",
+    "url": "/mcmasteraerospace/static/media/SolidWorks-Logo.8adb877f.png"
+  },
+  {
+    "revision": "62e504cc83d0f1d8feed6ce6dabe39a6",
+    "url": "/mcmasteraerospace/static/media/Aecon-Logo.wine.62e504cc.png"
+  },
+  {
+    "revision": "7dcc41a06c31d21e90b4ca421e9d0a86",
+    "url": "/mcmasteraerospace/static/media/ansys-logo.7dcc41a0.jpg"
+  },
+  {
+    "revision": "0df843898fa299dc199dc2c1e0680c8c",
+    "url": "/mcmasteraerospace/static/media/BCHughes-Colour-HR.0df84389.png"
+  },
+  {
+    "revision": "63e0f30c4ba3b68cedd7681e1292b76c",
+    "url": "/mcmasteraerospace/static/media/McMaster-Colour-Cropped.63e0f30c.png"
+  },
+  {
+    "revision": "0634a5babf36db50e415356b348e3f30",
+    "url": "/mcmasteraerospace/static/media/instagram.0634a5ba.png"
+  },
+  {
+    "revision": "10040a09610fbea3a75c1e8c6d09e152",
+    "url": "/mcmasteraerospace/static/media/mcm-eng_stack-blk_png.10040a09.png"
+  },
+  {
+    "revision": "a13814f4bafa52676a0cee79116543df",
+    "url": "/mcmasteraerospace/static/media/McMaster Aerospace Team Photo.a13814f4.jpg"
+  },
+  {
+    "revision": "326e5a31b7744bb5c10f78c4184087d8",
+    "url": "/mcmasteraerospace/static/media/prototype.326e5a31.png"
+  },
+  {
+    "revision": "719e30dc32334e157c405570765d1e2a",
+    "url": "/mcmasteraerospace/static/media/mastLogo.719e30dc.jpg"
+  },
+  {
+    "revision": "b5807635982a674c71ee82797cc656c6",
+    "url": "/mcmasteraerospace/static/media/McMaster - colour.b5807635.png"
+  },
+  {
+    "revision": "4b62058228920d32bfd2b33802235975",
+    "url": "/mcmasteraerospace/static/media/mastTeam.4b620582.jpg"
+  },
+  {
+    "revision": "008cbc9442c0052ff181caeb7d7e0ad3",
+    "url": "/mcmasteraerospace/static/media/MAST-colour.008cbc94.png"
+  },
+  {
+    "revision": "5aecaf7e6c8ff915e912848352e45bbd",
+    "url": "/mcmasteraerospace/static/media/MAST - colour.5aecaf7e.png"
+  },
+  {
+    "revision": "d5245984dfd140ecc3bf53d7494d9601",
+    "url": "/mcmasteraerospace/index.html"
+  }
+];
