803195c093ba7f1cb705faeecc508efc90f0f998
Jeremy Langner <langnerj@mcmaster.ca>
2022-10-18 09:48:41 -0400

    Updates

---
+++ precache-manifest.6861c77bae64e4c1e186943021c16e71.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dfdb391ecb8de8e3b82",
+    "url": "/static/css/main.14995c2b.chunk.css"
+  },
+  {
+    "revision": "6dfdb391ecb8de8e3b82",
+    "url": "/static/js/main.6dfdb391.chunk.js"
+  },
+  {
+    "revision": "bb2d15f8b100106d30b0",
+    "url": "/static/js/1.bb2d15f8.chunk.js"
+  },
+  {
+    "revision": "fcc9517fe3c2da6f93b9",
+    "url": "/static/css/2.f454a78b.chunk.css"
+  },
+  {
+    "revision": "fcc9517fe3c2da6f93b9",
+    "url": "/static/js/2.fcc9517f.chunk.js"
+  },
+  {
+    "revision": "2ff3dd98650ffb28a13e",
+    "url": "/static/js/runtime~main.2ff3dd98.js"
+  },
+  {
+    "revision": "860c0e74436a060d01c5a5feb945fa89",
+    "url": "/static/media/McMaster- plane-colour.860c0e74.png"
+  },
+  {
+    "revision": "ee761be490ce81562cebb66c419cc10b",
+    "url": "/static/media/McMaster-title-color.ee761be4.png"
+  },
+  {
+    "revision": "f2dcc57b196f37ae020cca170aa9e84f",
+    "url": "/static/media/SAE_International_logo.f2dcc57b.png"
+  },
+  {
+    "revision": "af9d7496fe52d1a100831f9a16cacd95",
+    "url": "/static/media/SAE_design_east.af9d7496.png"
+  },
+  {
+    "revision": "8363a0bcaa5d0a745f9833e2738826cb",
+    "url": "/static/media/around-the-world.8363a0bc.png"
+  },
+  {
+    "revision": "accae175d519c5dbd7095b25d95c63e6",
+    "url": "/static/media/team.accae175.png"
+  },
+  {
+    "revision": "8adb877f8c035c53823b7926e83d0119",
+    "url": "/static/media/SolidWorks-Logo.8adb877f.png"
+  },
+  {
+    "revision": "62e504cc83d0f1d8feed6ce6dabe39a6",
+    "url": "/static/media/Aecon-Logo.wine.62e504cc.png"
+  },
+  {
+    "revision": "7dcc41a06c31d21e90b4ca421e9d0a86",
+    "url": "/static/media/ansys-logo.7dcc41a0.jpg"
+  },
+  {
+    "revision": "0df843898fa299dc199dc2c1e0680c8c",
+    "url": "/static/media/BCHughes-Colour-HR.0df84389.png"
+  },
+  {
+    "revision": "63e0f30c4ba3b68cedd7681e1292b76c",
+    "url": "/static/media/McMaster-Colour-Cropped.63e0f30c.png"
+  },
+  {
+    "revision": "0634a5babf36db50e415356b348e3f30",
+    "url": "/static/media/instagram.0634a5ba.png"
+  },
+  {
+    "revision": "65c8ce096bb6e66b4c708f0ac0fdf0ef",
+    "url": "/static/media/mcm-eng_stack-col_png.65c8ce09.png"
+  },
+  {
+    "revision": "a13814f4bafa52676a0cee79116543df",
+    "url": "/static/media/McMaster Aerospace Team Photo.a13814f4.jpg"
+  },
+  {
+    "revision": "326e5a31b7744bb5c10f78c4184087d8",
+    "url": "/static/media/prototype.326e5a31.png"
+  },
+  {
+    "revision": "719e30dc32334e157c405570765d1e2a",
+    "url": "/static/media/mastLogo.719e30dc.jpg"
+  },
+  {
+    "revision": "b5807635982a674c71ee82797cc656c6",
+    "url": "/static/media/McMaster - colour.b5807635.png"
+  },
+  {
+    "revision": "4b62058228920d32bfd2b33802235975",
+    "url": "/static/media/mastTeam.4b620582.jpg"
+  },
+  {
+    "revision": "008cbc9442c0052ff181caeb7d7e0ad3",
+    "url": "/static/media/MAST-colour.008cbc94.png"
+  },
+  {
+    "revision": "5aecaf7e6c8ff915e912848352e45bbd",
+    "url": "/static/media/MAST - colour.5aecaf7e.png"
+  },
+  {
+    "revision": "3e7593e09c4179cc4e983641f4f8e0df",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mcmasteraerospace/precache-manifest.6fc820c953a7170effafa1b1bbd90eb9.js"
+  "/precache-manifest.6861c77bae64e4c1e186943021c16e71.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mcmasteraerospace/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
