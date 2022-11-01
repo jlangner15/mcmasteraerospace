a06c0322767ecb7a91d48787877066a6d5ecbf24
Jeremy Langner <langnerj@mcmaster.ca>
2022-10-31 23:21:08 -0400

    Added more sponsors and couple alignemnt fixes

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -5,7 +5,10 @@ import aecon from '../assets/sponsors/Aecon-Logo.wine.png'
 import ansys from '../assets/sponsors/ansys-logo.jpg'
 import bcHughes from '../assets/sponsors/BCHughes-Colour-HR.png'
 import stelco from '../assets/sponsors/stelco-inc-logo-vector.png'
-
+import skyGauge from '../assets/sponsors/Skygauge_Logo.jpg'
+import macEng from '../assets/sponsors/McMaster Engineering.jpg'
+import greatHobbies from '../assets/sponsors/GreatHobbies.png'
+import amp from '../assets/sponsors/AMP Logos_Green w- Words.png'
 
 
 function Sponsors() {
@@ -20,27 +23,49 @@ function Sponsors() {
 
         </div>
 
-        {/* <div class="sponsor-group" id="platnium-sponsors">
+        <div class="sponsor-group" id="platnium-sponsors">
             <div class="row">
-                <div class="column left33">
+                <div class="column left33" id="sponsor-triangle">
+                    <a class="triangle-bottomright black"></a>
+                </div>
+                <div class="column center33">
+                    <h6 class="title-sponsor">Title</h6>
+                </div>
+                <div class="column right33" id="sponsor-triangle">
+                    <a class="triangle-bottomleft black"></a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="center">
+                    <a href="https://www.eng.mcmaster.ca/" target="blank"> 
+                        <img src={macEng} class="title-sponsor-logo shad"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="sponsor-group" id="platnium-sponsors">
+            <div class="row">
+                <div class="column left33" id="sponsor-triangle">
                     <a class="triangle-bottomright platnium"></a>
                 </div>
                 <div class="column center33">
                     <h6 class="platnium-sponsor">Platnium</h6>
                 </div>
-                <div class="column right33">
+                <div class="column right33" id="sponsor-triangle">
                     <a class="triangle-bottomleft platnium"></a>
                 </div>
             </div>
 
             <div class="row">
                 <div class="center">
-                    <a href="https://www.solidworks.com/" target="blank"> 
-                        <img src={bombay} class="platnium-sponsor-logo shad"/>
+                    <a href="https://www.skygauge.co/" target="blank"> 
+                        <img src={skyGauge} class="platnium-sponsor-logo shad"/>
                     </a>
                 </div>
             </div>
-        </div> */}
+        </div>
 
 
         <div class="sponsor-group" id="gold-sponsors">
@@ -49,7 +74,7 @@ function Sponsors() {
                     <a class="triangle-bottomright gold"></a>
                 </div>
                 <div class="column center33">
-                    <h6 class="platnium-sponsor">Gold</h6>
+                    <h6 class="gold-sponsor">Gold</h6>
                 </div>
                 <div class="column right33" id="sponsor-triangle">
                     <a class="triangle-bottomleft gold"></a>
@@ -73,19 +98,19 @@ function Sponsors() {
 
         <div class="sponsor-group" id="silver-sponsors">
             <div class="row">
-                <div class="column left33" >
+                <div class="column left33" id="sponsor-triangle">
                     <a class="triangle-bottomright silver"></a>
                 </div>
                 <div class="column center33">
-                    <h6 class="platnium-sponsor">Silver</h6>
+                    <h6 class="silv-bronz-sponsor">Silver</h6>
                 </div>
-                <div class="column right33">
+                <div class="column right33" id="sponsor-triangle">
                     <a class="triangle-bottomleft silver"></a>
                 </div>
             </div>
 
             <div class="row">
-                <div class="column left33">
+                <div class="column left33" >
                     <a href="https://www.ansys.com/" target="blank"> 
                         <img src={ansys} class="silver-sponsor-logo shad"/>
                     </a>
@@ -95,31 +120,31 @@ function Sponsors() {
                         <img src={stelco} class="silver-sponsor-logo shad"/>
                     </a>
                 </div>
-                {/* <div class="column right33">
-                    <a href="" target="blank"> 
-                        <img src={} class="silver-sponsor-logo shad"/>
+                <div class="column right33">
+                    <a href="https://www.advancedmobilityproducts.com/" target="blank"> 
+                        <img src={amp} class="silver-sponsor-logo shad"/>
                     </a>
-                </div> */}
+                </div>
             </div>
         </div>
 
         <div class="sponsor-group" id="bronze-sponsors">
             <div class="row" id="bronze-sponsors">
-                <div class="column left33">
+                <div class="column left33" id="sponsor-triangle">
                     <a class="triangle-bottomright bronze"></a>
                 </div>
                 <div class="column center33">
-                    <h6 class="platnium-sponsor">Bronze</h6>
+                    <h6 class="silv-bronz-sponsor">Bronze</h6>
                 </div>
-                <div class="column right33">
+                <div class="column right33" id="sponsor-triangle">
                     <a class="triangle-bottomleft bronze"></a>
                 </div>
             </div>
 
             <div class="row">
                 <div class="column left33">
-                    <a target="blank"> 
-                        {/*<img src={ansys} class="silver-sponsor-logo shad"/>*/}
+                    <a href="https://www.greathobbies.com/" target="blank"> 
+                        <img src={greatHobbies} class="bronze-sponsor-logo shad"/>
                     </a>
                 </div>
                 <div class="column center33">
